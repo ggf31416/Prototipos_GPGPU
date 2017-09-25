7154cf55f41f44abe0e39270c50601d4eabc56ff
Guillermo Gabrielli <ggf31416@gmail.com>
2017-09-25 18:45:23 -0300

    Renamed defines_onemax -> defines_knapsack

---
+++ Knapsack/Knapsack/Knapsack/defines_knapsack.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "stdint.h"
+
+#define INIT_THREADS 128
+#define MAX_THREADS_PER_BLOCK 512
+#define MAX_WEIGHT 1000 // peso maximo de la mochila
+#define PENAL 1 // valor penalizacion sobrepeso
+#define T_FIT float // tipo de datos de fitness
+
+#define SALIDA 1
+#define SALIDA_STEP 500 // cada cuanto mostrar estadisticas
+#define ORDEN_DPX 1 // forzar pnt2 > pnt 1
+
+// para bitwise
+// The constant DataSize should be defined as 8 (if Data is unsigned char), 16 (if Data is unsigned short), 
+// 32 (if Data is unsigned int) or 64 (if Data is unsigned long).
+#define DataSize 64
+
+// The datatype Data should be defined as unsigned char, unsigned short, unsigned int or unsigned long.
+#if DataSize == 8
+typedef unsigned char Data;
+
+#elif DataSize == 16
+typedef uint16_t Data;
+
+#elif DataSize == 32
+typedef uint32_t Data;
+
+#else 
+typedef uint64_t Data;
+
+#endif // DataSize == 8
+
